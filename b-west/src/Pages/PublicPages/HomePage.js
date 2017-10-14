ec255fc27e1d9428427e2e7b9450ff61e73c189a
koookiii <chriss.hadad@gmail.com>
2017-10-14 10:28:29 +0300

    removed  menu nav vertical scroll bar

--- b-west/src/Pages/PublicPages/HomePage.js
+++ b-west/src/Pages/PublicPages/HomePage.js
@@ -7,7 +7,8 @@ import Store from "../../components/Store";
 import SmallSlider from "../../components/SmallSlider";
 import Header, {Header2, Header3} from "../../components/Header";
 import Banner from "../../components/Banner";
-import '../../components/Footer.css'
+
+
 
 class HomePage extends React.Component {
 
@@ -135,63 +136,9 @@ class HomePage extends React.Component {
                 </div>
                 {/* End Content */}
                 {/* Footer */}
-                <footer className="footer">
-                    <div className="footer-container">
-                        <ul className="footer-social__items">
-                            <li className="footer-social__item">
-                                <a className=" social_link" href="http://instagram.com/villagergoods"
-                                   target="_blank">
-                                    <i className={'fa fa-instagram'}/>
-                                </a>
-                            </li>
-                            <li className="footer-social__item">
-                                <a className="social_link" href="http://facebook.com/villagergoods"
-                                   target="_blank">
-                                    <i className={'fa fa-facebook'}/>
-                                </a>
-                            </li>
-                        </ul>
-                        <ul className="footer__links">
-                            <li className="footer__list-item">
-                                <a href="/pages/coconut-water" className="footer__link">
-                                    Coconut Water
-                                </a>
-                            </li>
-                            <li className="footer__list-item">
-                                <a href="/collections/shop" className="footer__link">
-                                    Shop
-                                </a>
-                            </li>
-                            <li className="footer__list-item">
-                                <a href="/pages/contact" className="footer__link">
-                                    Contact
-                                </a>
-                            </li>
-                            <li className="footer__list-item">
-                                <a href="/pages/terms-of-service" className="footer__link">
-                                    Terms of Use
-                                </a>
-                            </li>
-                            <li className="footer__list-item">
-                                <a href="/pages/privacy-policy" className="footer__link">
-                                    Privacy Policy
-                                </a>
-                            </li>
-                            <li className="footer__list-item">
-                                <a href="/pages/faq" className="footer__link">
-                                    FAQs
-                                </a>
-                            </li>
 
-                        </ul>
+                
 
-                        <p className="footer__credit">
-                            <a href="http://department.nyc" className="footer__credit-href" target="_blank">
-                                Site Credit: Department
-                            </a>
-                        </p>
-                    </div>
-                </footer>
                 {/* End Footer */}
                 {/* GO TO TOP */}
                 <a className="cd-top"><i className="fa fa-angle-up"/></a>
