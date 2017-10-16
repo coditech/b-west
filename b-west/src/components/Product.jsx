f8c47b0816c2e26fcc70180c59aec44a193eed1a
koookiii <chriss.hadad@gmail.com>
2017-10-16 14:52:03 +0300

    Product component

---
+++ b-west/src/components/Product.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/Store.css";
+
+const Product = ({ status, productUrl, name, price, image }) => {
+  return (
+    <li className="col-sm-4">
+      <div className="items-in">
+        {status ? <div className="shop-tags">{status}</div> : ""}
+        <a href={productUrl}>
+          <img src={image.url} alt={image.url} />
+        </a>
+      </div>
+      <div className="details-sec">
+        <a href={productUrl}> {name} </a>
+        <span className="font-crimson font-italic font-18px">{price}</span>
+      </div>
+    </li>
+  );
+};
+
+export default Product;
