a3d7714302a8f22e3e05ce2eb175b0d23553c0fd
Gaby Karam <gkaramgk94@gmail.com>
2017-10-16 17:30:55 +0300

    replace image.url with image.src

--- b-west/src/components/Store.jsx
+++ b-west/src/components/Store.jsx
@@ -3,61 +3,61 @@ import "../styles/Store.css";
 import Product from "./Product";
 
 const dataTest = [
-  {
-    image: {
-      url: "/product_1.jpg",
-      alt: "img1"
+    {
+        image: {
+            src: "/product_1.jpg",
+            alt: "img1"
+        },
+        name: "Rose Jam",
+        price: "$79.00",
+        productUrl: "/url1",
+        status: "NEW"
     },
-    name: "Rose Jam",
-    price: "$79.00",
-    productUrl: "/url1",
-    status: "NEW"
-  },
-  {
-    image: {
-      url: "/product_2.jpg",
-      alt: "img2"
+    {
+        image: {
+            src: "/product_2.jpg",
+            alt: "img2"
+        },
+        name: "JACQUARD FLORAL COAT",
+        price: "$100.00",
+        productUrl: "/url2",
+        status: ""
     },
-    name: "JACQUARD FLORAL COAT",
-    price: "$100.00",
-    productUrl: "/url2",
-    status: ""
-  },
-  {
-    image: {
-      url: "/product_3.jpg",
-      alt: "img3"
-    },
-    name: "LONG CHECKED OVERSHIRT",
-    price: "$700.00",
-    productUrl: "/url3",
-    status: "OLD"
-  }
+    {
+        image: {
+            src: "/product_3.jpg",
+            alt: "img3"
+        },
+        name: "LONG CHECKED OVERSHIRT",
+        price: "$700.00",
+        productUrl: "/url3",
+        status: "OLD"
+    }
 ];
 
 const Store = () => {
-  return (
-    <div className="container">
-      <div className="popurlar_product">
-        <ul className="row">
-          {dataTest.map(
-            (storeItem, n) =>
-              n < 3 ? (
-                <Product
-                  name={storeItem.name}
-                  url={storeItem.productUrl}
-                  image={storeItem.image}
-                  price={storeItem.price}
-                  status={storeItem.status}
-                  classContainer={"col-xs-4"}
-                  key={n}
-                />
-              ) : null
-          )}
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="popurlar_product">
+                <ul className="row">
+                    {dataTest.map(
+                        (storeItem, n) =>
+                            n < 3 ? (
+                                <Product
+                                    name={storeItem.name}
+                                    url={storeItem.productUrl}
+                                    image={storeItem.image}
+                                    price={storeItem.price}
+                                    status={storeItem.status}
+                                    classContainer={"col-xs-4"}
+                                    key={n}
+                                />
+                            ) : null
+                    )}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Store;
