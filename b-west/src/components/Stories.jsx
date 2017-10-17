3280cc23cc0c18bda6230cb8b8f224cf10977a90
koookiii <chriss.hadad@gmail.com>
2017-10-17 16:17:33 +0300

    Stories fix on mobile

--- b-west/src/components/Stories.jsx
+++ b-west/src/components/Stories.jsx
@@ -1,64 +1,96 @@
 import React from "react";
-import '../styles/Stories.css'
-import {NavLink} from "react-router-dom";
+import "../styles/Stories.css";
+import { NavLink } from "react-router-dom";
 
-const Stories = ({firstStory, secondStory}) => {
-    return (
-        <div>
-            <section className="padding-top-50 padding-bottom-80">
-                <div className="container">
-                    <div className="intro-5 margin-top-20">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="heading-block no-margin-bottom margin-top-0"><span
-                                    className="margin-bottom-15 margin-top-20">{firstStory.slogan}</span>
-                                    <h2 className="margin-bottom-20 margin-top-1">{firstStory.title}</h2>
-                                </div>
-                                <p>{firstStory.desc}</p>
-                                <NavLink to={'/villagers#' + firstStory.slug}
-                                         className={'btn btn-large dark-border font-normal margin-top-50 letter-space-1'}>
-                                    READ&nbsp;MORE
-                                </NavLink>
-                            </div>
-
-                            <div className="col-md-6"><img className="img-responsive" src={firstStory.image}
-                                                           alt={firstStory.alt}/></div>
-                        </div>
-                    </div>
-
-                    <hr/>
-                    <div className="intro-5 margin-top-20">
-                        <div className="row">
-
-
-                            <div className="col-md-6">
-                                <img className="img-responsive" src={secondStory.image}
-                                     alt={secondStory.alt}/>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="heading-block no-margin-bottom margin-top-0"><span
-                                    className="margin-bottom-15">{secondStory.slogan}</span>
-                                    <h2 className="margin-bottom-20 margin-top-1">{secondStory.title}</h2>
-                                </div>
-                                <p>{secondStory.desc}</p>
-                                <NavLink to={'/villagers#' + secondStory.slug}
-                                         className={'btn btn-large dark-border font-normal margin-top-50 letter-space-1'}>
-                                    READ&nbsp;MORE
-                                </NavLink>
-                            </div>
-                        </div>
-                    </div>
+const Stories = ({ firstStory, secondStory }) => {
+  return (
+    <div>
+      <section className="padding-top-50 padding-bottom-80">
+        <div className="container">
+          <div className="intro-5 margin-top-20">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="heading-block no-margin-bottom margin-top-0">
+                  <span className="margin-bottom-15 margin-top-20">
+                    {firstStory.slogan}
+                  </span>
+                  <h2 className="margin-bottom-20 margin-top-1">
+                    {firstStory.title}
+                  </h2>
                 </div>
+                <p>{firstStory.desc}</p>
+                <NavLink
+                  to={"/villagers#" + firstStory.slug}
+                  className={
+                    "btn btn-large dark-border font-normal margin-top-35 letter-space-1 margin-bottom-50"
+                  }
+                >
+                  READ&nbsp;MORE
+                </NavLink>
+              </div>
 
-            </section>
-            <section className={'promo dark dark-pattern'} style={{textAlign: "center"}}>
-                <div className="container text-center">
-                    <NavLink to={'/villagers#wrap'} className={'btn btn-color btn-med'}>More Villagers Stories</NavLink>
+              <div className="col-md-6">
+                <img
+                  className="img-responsive"
+                  src={firstStory.image}
+                  alt={firstStory.alt}
+                />
+              </div>
+            </div>
+          </div>
+
+          <hr />
+          <div className="intro-5 margin-top-20">
+            <div className="row">
+              <div className="col-md-6 hidden-xs hidden-sm">
+                <img
+                  className="img-responsive"
+                  src={secondStory.image}
+                  alt={secondStory.alt}
+                />
+              </div>
+              <div className="col-md-6 ">
+                <div className="heading-block no-margin-bottom margin-top-0">
+                  <span className="margin-bottom-15">{secondStory.slogan}</span>
+                  <h2 className="margin-bottom-20 margin-top-1">
+                    {secondStory.title}
+                  </h2>
                 </div>
-            </section>
+                <p>{secondStory.desc}</p>
+                <NavLink
+                  to={"/villagers#" + secondStory.slug}
+                  className={
+                    "btn btn-large dark-border font-normal margin-top-35 letter-space-1 margin-bottom-50"
+                  }
+                >
+                  READ&nbsp;MORE
+                </NavLink>
+              </div>
+
+              <div className="col-md-6 hidden-md hidden-lg">
+                <img
+                  className="img-responsive"
+                  src={secondStory.image}
+                  alt={secondStory.alt}
+                />
+              </div>
+
+            </div>
+          </div>
         </div>
-
-    );
+      </section>
+      <section
+        className={"promo dark dark-pattern"}
+        style={{ textAlign: "center" }}
+      >
+        <div className="container text-center">
+          <NavLink to={"/villagers#wrap"} className={"btn btn-color btn-med"}>
+            More Villagers Stories
+          </NavLink>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Stories;
