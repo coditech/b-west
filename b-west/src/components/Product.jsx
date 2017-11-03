bb0ba3ea64ebef36eadd000ff43585554e2a8b90
Gaby Karam <gkaramgk94@gmail.com>
2017-11-03 13:23:07 +0200

    removing back lazy load
    Took 2 hours 14 minutes

--- b-west/src/components/Product.jsx
+++ b-west/src/components/Product.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
-import LazyLoad from "react-lazyload";
 import "../styles/Store.css";
 
 const Product = (props) => {
@@ -10,10 +9,8 @@ const Product = (props) => {
             <div className="items-in">
                 {status ? <div className="shop-tags">{status}</div> : ""}
                 <NavLink to={'/products/' + slug}>
-                    <LazyLoad height={200} offset={100}>
 
                         <img src={imageSrc} alt={imageAlt}/>
-                    </LazyLoad>
 
                 </NavLink>
             </div>
